ad6550b9cfa4284cc935ab8782ce687bd6539cff
Thiago Porto <thiago@waib.com.br>
2014-05-20 11:51:33 -0300

    ajustes na estrutura

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 skeleton-express4
 =================
 
-Estrutura básica para trabalhar com express 4
+Estrutura básica para trabalhar com o framework express na sua versão 4
+<br>
+Criado com base no módulo <a href="https://www.npmjs.org/package/express-generator">express-generator</a>
+<br>
+<br>
+<h3>Instalação</h3>
+<p>Faça o clone do repositório, acesse-o e execute npm install, para instalar as dependências.</p>
--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('static-favicon');
-var logger = require('morgan');
+var express      = require('express');
+var path         = require('path');
+var favicon      = require('static-favicon');
+var logger       = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var bodyParser   = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var users  = require('./routes/users');
 
 var app = express();
 
@@ -56,4 +56,6 @@ app.use(function(err, req, res, next) {
 });
 
 
-module.exports = app;
+app.listen(3000, function() {
+    console.log('Express server listening on port 3000');
+});
