77293acda8546af42ec144c54421c33f23112787
Thiago Porto <thiago@waib.com.br>
2014-05-23 16:11:25 -0300

    criado os middleware not-found e server error 500

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ var users  = require('./routes/users');
 
 var app = express();
 
+//middleware
+var erros = require('./middleware/erros');
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -20,43 +23,15 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
-app.use(session({ secret: 'sua-chave-secreta'}));
+app.use(session({ secret: 'sua-chave-secreta' }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
 
-/// catch 404 and forwarding to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-/// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
-
+//middleware
+app.use(erros.notfound);
+app.use(erros.serverError);
 
 app.listen(3000, function() {
     console.log('Express server listening on port 3000');
